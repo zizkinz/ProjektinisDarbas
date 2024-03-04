a3922fdf03ebedaaca6490d93105e35c4c1052c9
Maksim Cizov <74565069+zizkinz@users.noreply.github.com>
2024-03-04 14:48:43 +0200

    Add files via upload

---
+++ main.R
@@ -0,0 +1,29 @@
+library(Rsamtools)
+filename <-  'test_data/DP23_sequence.bam'
+utils:::format.object_size(file.info(filename)$size, "auto")
+
+bamFile <- BamFile(filename)
+seqinfo(bamFile)
+
+df <- as.data.frame(seqinfo(bamFile))
+head(df)
+#
+bamFile
+seqlevels(bamFile)
+
+genome(bamFile)
+
+BamViews(bamFile)
+
+which <- GRanges("9:107987096-107993120")
+param <- ScanBamParam(which=which, what=scanBamWhat())
+sq <- scanBam(bamFile, param=param)
+
+seqlevels(bamFile)
+
+
+ss <- sq$`9:107987096-107993120`$seq
+library(stringr)
+z <- str_split_1(as.character(ss[1]), pattern = '')
+barplot(table(z))
+

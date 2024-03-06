27d838191bc5d69218de4199b4181d3cae587fc1
maksimcizov <maksimcizov@gmail.com>
2024-03-06 20:30:44 +0200

    g

--- main.R
+++ main.R
@@ -31,59 +31,62 @@ ks <- function(hyp_dist, covmx){
 }
 
 fn <- function (x) {
+  x <- na.omit(x)
   return (-mean(x))
 }
 
-nsamples <- 2
+
 ngenes <- 8
 
+
+# -17 -11 -10
+# filelist <- c("test_data/DP5.bam","test_data/DP12.bam", "test_data/DP13.bam", "test_data/DP14.bam", "test_data/DP15.bam", "test_data/DP16.bam", "test_data/DP18.bam", "test_data/DP19.bam", "test_data/DP20.bam", "test_data/DP21.bam", "test_data/DP22.bam", "test_data/DP23.bam", "test_data/DP24.bam", "test_data/DP25.bam")
+
+nsamples <- 25
 ks_mx <- matrix(ncol = ngenes, nrow = nsamples)
-mrins <- numeric(nsamples)
-
-filelist <- c("test_data/DP23_sequence.bam", "test_data/DP24_sequence.bam")
-
+nms <- as.data.frame(read.table("test_data/Genes.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote=""))$V4
+colnames(ks_mx) <- nms
 for (j in 1:nsamples){
-  trm<-CoverageBamFile(filelist[j])
+  trm<-CoverageBamFile(paste("test_data/DP",j,".bam",sep = ''))
   # trm<-CoverageBamFile("test_data/DP23_sequence.bam")
   # trm<-CoverageBamFile("test_data/DP24_sequence.bam")
 
   n_wind <- 100
   covmx<-cov.matrix(trm, coordfile= "test_data/Genes.bed", bin_width=1, no_windows = n_wind)
+
   # write.profile(DP23_Glul_covmx,outfile="DP23_sequence_Glu.txt")
-  draw.profile(covmx,ylab="avg coverage",outfile=paste(j, ".png", sep = ''))
+  # draw.profile(covmx,ylab="avg coverage",outfile=paste(j, ".png", sep = ''))
   # draw.heatmap(covmx,outfile="DP23_sequence_Glu_heatmap.png")
 
-  # plot(1:nrow(covmx),cumsum(covmx[,2]/sum(covmx[,2])), type = "l")
+  # plot(1:nrow(covmx),cumsum(covmx[,6]/sum(covmx[,6])), type = "l")
   # lines(1:nrow(covmx), punif(1:nrow(covmx),0,100), type = "l")
 
   ks_mx[j,] <- ks(hyp_dist, covmx)
 }
 
-ks_norm <- ks_mx - median(ks_mx)
+ks_norm <- ks_mx - median(ks_mx,na.rm = TRUE)
 mrin <- apply(ks_norm, MARGIN = 1, FUN = fn)
+print(mrin)
 
 # fitting mrins
 hist(mrin)
 
+fitdistr(mrin,densfun = "normal")$sd
 
+#       mean         sd
+# 0.01935427 0.01368553
 
+qqnorm(mrin, pch = 16)
+qqline(mrin)
 
-#
-# l <- fitdistr(ks_norm,densfun = "normal")$sd
-#  #      mean          sd
-#  #  0.05126011   0.20428576
-#  # (0.07222592) (0.05107144)
-#
-#
-#
-# qqnorm(ks_norm, pch = 16)
-# qqline(ks_norm)
-# ks_norm
-#
-# plot(ecdf(ks_norm))
-#
-# error <- qnorm(0.995)*as.numeric(l[2])/sqrt(n_wind)
+# error <- qnorm(0.995) * as.numeric(l[2])/sqrt(n_wind)
 
+conf_int <- c(as.numeric(l[1]) - error, as.numeric(l[1]) + error)
+
+plot(ecdf(mrin))
+abline(v=l[1], col = "red")
+abline(v=l[1] - 2 * l[2], col = "blue")
+abline(v=l[1] + 2 * l[2], col = "blue")
 
 
 #   dat <- read.table("test_data/DP23_sequence_Glul.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote="")

cf74926a7b50178bc5e050a85d284da124c2b1f3
maksimcizov <maksimcizov@gmail.com>
2024-03-07 20:46:52 +0200

    g

--- main.R
+++ main.R
@@ -1,5 +1,7 @@
 library(CoverageView)
 library(MASS)
+library(plot.matrix)
+library(RColorBrewer)
 
 hyp_dist <- function(x){
   return(punif(x,0,n_wind))
@@ -30,33 +32,27 @@ ks <- function(hyp_dist, covmx){
   return(ks_d)
 }
 
-fn <- function (x) {
-  x <- na.omit(x)
-  return (-mean(x))
-}
-
-
 ngenes <- 8
-
-
-# -17 -11 -10
-# filelist <- c("test_data/DP5.bam","test_data/DP12.bam", "test_data/DP13.bam", "test_data/DP14.bam", "test_data/DP15.bam", "test_data/DP16.bam", "test_data/DP18.bam", "test_data/DP19.bam", "test_data/DP20.bam", "test_data/DP21.bam", "test_data/DP22.bam", "test_data/DP23.bam", "test_data/DP24.bam", "test_data/DP25.bam")
-
 nsamples <- 25
+n_wind <- 100
+
 ks_mx <- matrix(ncol = ngenes, nrow = nsamples)
 nms <- as.data.frame(read.table("test_data/Genes.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote=""))$V4
+# k <- as.data.frame(read.table("test_data/AllGenes.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote=""))
 colnames(ks_mx) <- nms
 for (j in 1:nsamples){
   trm<-CoverageBamFile(paste("test_data/DP",j,".bam",sep = ''))
   # trm<-CoverageBamFile("test_data/DP23_sequence.bam")
   # trm<-CoverageBamFile("test_data/DP24_sequence.bam")
 
-  n_wind <- 100
+
   covmx<-cov.matrix(trm, coordfile= "test_data/Genes.bed", bin_width=1, no_windows = n_wind)
 
   # write.profile(DP23_Glul_covmx,outfile="DP23_sequence_Glu.txt")
   # draw.profile(covmx,ylab="avg coverage",outfile=paste(j, ".png", sep = ''))
+  # draw.profile(covmx,ylab="avg coverage",outfile=paste(j, "_Aldh1l1.png", sep = ''))
   # draw.heatmap(covmx,outfile="DP23_sequence_Glu_heatmap.png")
+  # barplot((covmx[,5]))
 
   # plot(1:nrow(covmx),cumsum(covmx[,6]/sum(covmx[,6])), type = "l")
   # lines(1:nrow(covmx), punif(1:nrow(covmx),0,100), type = "l")
@@ -64,34 +60,45 @@ for (j in 1:nsamples){
   ks_mx[j,] <- ks(hyp_dist, covmx)
 }
 
-ks_norm <- ks_mx - median(ks_mx,na.rm = TRUE)
-mrin <- apply(ks_norm, MARGIN = 1, FUN = fn)
+medians <- apply(ks_mx, MARGIN = 2, FUN = function (x) rep(median(x, na.rm = TRUE),times = length(x)))
+ks_norm <- ks_mx-medians
+
+mrin <- apply(ks_norm, MARGIN = 1, FUN = function (x) -mean(na.omit(x)))
+options(scipen=999)
 print(mrin)
+
+# mKS matrix
+par(mar=c(5.1, 4.1, 4.1, 4.1))
+plot(ks_norm,asp = FALSE,cex= 0.8)
+plot(ks_norm,breaks = c(-1,-0.2,-0.05,-0.02,-0.01,0,0.01,0.02,0.05,0.2,1), col=brewer.pal(10,"RdBu"), na.col="white")
 
 # fitting mrins
 hist(mrin)
+mean(mrin)
 
-fitdistr(mrin,densfun = "normal")$sd
+l <- fitdistr(mrin,densfun = "normal")$sd
+l
 
 #       mean         sd
-# 0.01935427 0.01368553
+# 0.01643934 0.01162437
 
-qqnorm(mrin, pch = 16)
+
+qqnorm(mrin)
 qqline(mrin)
 
-# error <- qnorm(0.995) * as.numeric(l[2])/sqrt(n_wind)
 
+error <- qnorm(0.975,mean = l[1], sd = l[2])
 conf_int <- c(as.numeric(l[1]) - error, as.numeric(l[1]) + error)
+which(mrin < conf_int[1])
+# 1  2  3  4  6  8  9 10 11 12
+
+shapiro.test(mrin)
+#       mean         sd
+# 0.01643934 0.01162437
 
 plot(ecdf(mrin))
 abline(v=l[1], col = "red")
-abline(v=l[1] - 2 * l[2], col = "blue")
-abline(v=l[1] + 2 * l[2], col = "blue")
-
-
-#   dat <- read.table("test_data/DP23_sequence_Glul.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote="")
-# hist((dat$V2), breaks = 100, freq = F)
-# hist(runif(nrow(dat), min = min(dat$V2), max = max(dat$V2)))
-# ks.test(dat$V2, 'punif',min(dat$V2), max(dat$V2))
+abline(v=conf_int[1], col = "blue")
+abline(v=conf_int[2], col = "blue")
 
 
